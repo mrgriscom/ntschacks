d63b3e8205236a735e454f68c92816c3f6ad7900
Drew Roos <mrgriscom@gmail.com>
2019-08-12 12:51:01 -0400

    works

--- blah.py
+++ blah.py
@@ -33,29 +33,75 @@ def within(pct, epsilon=.005):
     return 1. - epsilon < pct < 1. + epsilon
             
 last_h = None
+hsyncs = []
 for i, s in enumerate(syncs()):
-    if last_h:
-        elapsed = s - last
-        
+    #if s > 4e6: # just process beginning to start
+    #    break
 
-        
-        k = elapsed/63.55555
-        epsilon = .005
-        if not ((.5 - epsilon < k < .5 + epsilon) or (1 - epsilon < k < 1 + epsilon)):
-            print i, elapsed
-        
-        #print i, elapsed
-    last_h = s
+    if not last_h:
+        last_h = s
+        continue
 
-"""
+    elapsed = s - last_h
+    if within(elapsed / 63.555):
+        hsyncs.append(s)
+        last_h = s
+    elif elapsed > 63.555:
+        print 'sync lost'
+        last_h = s
+
+front_porch = 1.5
+back_porch = 9.4  # includes sync pulse
+padding = .005*63.555
+#padding = .015*63.555
+
+sync_level = -920
+blank_level = -690
+ire = (blank_level - sync_level) / 40.
+white_level = blank_level + 100*ire
+
+sync_ix = 0
+def calc_sync():
+    sync_us = hsyncs[sync_ix]
+    return (sync_us - front_porch - padding) * 10.5, (sync_us + back_porch + padding) * 10.5
+sync_start, sync_end = calc_sync()
 x = []
 y = []
-for i, s in enumerate(samples()):
-    if i == 10000000:
-        break
-    x.append(i/10.5)
-    y.append(s)   
- 
+
+out1 = open('cvbs-scram.i16', 'w')
+out2 = open('cvbs-scram-inv.i16', 'w')
+
+#duty_cycle = [2, 2]
+duty_cycle = [0, 2]
+def scramble(t):
+    t *= 1e-6
+    return t % (duty_cycle[0] + duty_cycle[1]) > duty_cycle[0]
+
+for i, val in enumerate(samples()):
+    val1 = val
+    val2 = val
+    val = None
+    
+    t = i/10.5
+    #x.append(t)
+    if scramble(t):
+        if i > sync_start:
+            val1 = min(val1 + 80*ire, white_level)
+            val2 = min(val2 + 47.5*ire, white_level)
+        # invert
+        val2 = min(white_level - (val2 - (blank_level + 7.5*ire)), white_level)
+            
+    #y.append(val)
+    out1.write(struct.pack('<H', int(round(-val1))))
+    out2.write(struct.pack('<H', int(round(-val2))))
+    
+    if i > sync_end:
+        sync_ix += 1
+        if sync_ix == len(hsyncs):
+            break
+        sync_start, sync_end = calc_sync()
+
+"""
 fig, ax = plt.subplots()
 ax.plot(x, y)
 
