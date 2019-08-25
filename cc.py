12628bf756f8d14615fbc0206fcbc671d1800dd5
Drew Roos <mrgriscom@gmail.com>
2019-08-26 00:16:42 +0300

    tweaking for gladiator vhs

--- cc.py
+++ cc.py
@@ -13,11 +13,13 @@ samp_rate = 10.5
 #blank_level = -2915
 #sync_level = -1670
 #blank_level = -1265
-sync_level = -7625
-blank_level = -5735
+#sync_level = -7625
+#blank_level = -5735
+sync_level = -9590
+blank_level = -7750
 
 def samples():
-    with open('fw.i16') as f:
+    with open('gladiator.i16') as f:
         while True:
             buf = f.read(2)
             if not buf:
@@ -51,14 +53,14 @@ hsyncs = []
 line = None
 for i, s in enumerate(syncs()):
     t, dur = s
-    #if t > 2.5e6: # just process beginning to start
-    #    break
+    if t > 2.5e6: # just process beginning to start
+        break
 
-    if within(dur / 4.7, .025):
+    if within(dur / 4.7, .15):
         type = 'h'
-    elif within(dur / 2.35, .05):
+    elif within(dur / 2.35, .60):
         type = 'eq'
-    elif within(dur / 27.0):
+    elif within(dur / 27.0, .02):
         type = 'v'
     else:
         type = None
@@ -70,12 +72,14 @@ for i, s in enumerate(syncs()):
         continue
 
     elapsed = t - last_h[0]
-    if within(elapsed / 63.555):
+    #print dur, type, elapsed
+    if within(elapsed / 63.555, .15):
         if line is not None:
             line += 1
         if type == 'eq' and last_sync[1] == 'h':
             # TODO verify expected line
             line = 0
+        print t, line
         hsyncs.append((t, line))
         last_h = s
     elif elapsed > 63.555:
@@ -93,7 +97,7 @@ def _ire(x):
     return blank_level + x*ire
 white_level = _ire(100)
 
-hsyncs = [hs for hs in hsyncs if hs[1] in (20, 263+20)]
+#hsyncs = [hs for hs in hsyncs if hs[1] in (20, 262+20)] # 262 or 263?
 
 STREAM =    '\x14\x29'
 CLEAR_BUF = '\x14\x2e'
@@ -158,8 +162,8 @@ def calc_sync():
         bits = map(int, list(reversed(('0'*7)+bin(ord(b))[2:]))[:7])
         return bits + [(sum(bits)+1) % 2]
     bits = [0,0,1] + byte_to_bits(data[0]) + byte_to_bits(data[1])
-    print data, bits
-    
+    #print data, bits
+
     return (sync_us + back_porch) * samp_rate, (sync_us + 63.555 - front_porch) * samp_rate, bits
 sync_start, sync_end, cc_data = calc_sync()
 buf = []
@@ -176,15 +180,15 @@ for i, val in enumerate(samples()):
     t = i/samp_rate
 
     if i > sync_start:
-        clock = (i - sync_start) / ((63.555 - front_porch - back_porch) * samp_rate) * 26
-        if clock < 7:
-            val = .5*(blank_level+cc_level) + .5*(blank_level-cc_level)*math.cos(clock * 2*math.pi)
-        else:
-            val = (cc_level if (cc_data+[0])[int(clock)-7] else blank_level)
+        #clock = (i - sync_start) / ((63.555 - front_porch - back_porch) * samp_rate) * 26
+        #if clock < 7:
+        #    val = .5*(blank_level+cc_level) + .5*(blank_level-cc_level)*math.cos(clock * 2*math.pi)
+        #else:
+        #    val = (cc_level if (cc_data+[0])[int(clock)-7] else blank_level)
         buf.append(val)
 
-    out.write(struct.pack('<H', int(round(-val))))
-        
+    #out.write(struct.pack('<H', int(round(-val))))
+ 
     if i > sync_end:
         """
         fig, ax = plt.subplots()
@@ -194,16 +198,22 @@ for i, val in enumerate(samples()):
         ax.grid()
         print hsyncs[sync_ix][1]
         plt.show()
-
-        bits = [1 if v > blank_level+25*ire else 0 for v in [buf[int(37+27.833*(7+i-.3))] for i in xrange(19)]]
-        if bits[:3] != [0,0,1] or sum(bits[3:11]) % 2 != 1 or sum(bits[11:19]) % 2 != 1:
-            print 'checksum fail', bits
-
-        for offset in (3, 11):
-            val = reduce(lambda a,b: 2*a+b, reversed(bits[offset:offset+7]))
-            if val != 0:
-                print chr(val), '0x%02x' % val, (('0'*7)+bin(val)[2:])[-8:], hsyncs[sync_ix][1]
         """
+
+        print len(buf)
+        
+        if hsyncs[sync_ix][1] in (20, 262+20)]: # 262 or 263?
+            #bitaddr = lambda i: (37+27.833*(7+i-.3)) / 14. * samp_rate
+            bitaddr = lambda i: (14.888+1.986*i)*samp_rate
+            bits = [1 if v > blank_level+25*ire else 0 for v in [buf[int(bitaddr(i))] for i in xrange(19)]]
+            if not all(b == 0 for b in bits) and (bits[:3] != [0,0,1] or sum(bits[3:11]) % 2 != 1 or sum(bits[11:19]) % 2 != 1):
+                print 'checksum fail', bits
+
+            for offset in (3, 11):
+                val = reduce(lambda a,b: 2*a+b, reversed(bits[offset:offset+7]))
+                if val != 0:
+                    print chr(val), '0x%02x' % val, (('0'*7)+bin(val)[2:])[-8:], hsyncs[sync_ix][1]
+
         buf = []
 
         sync_ix += 1
